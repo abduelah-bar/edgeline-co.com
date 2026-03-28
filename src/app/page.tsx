75853565c9e0859983fa3e8403a5fa8c0cd94a8f
onlyAbood <Baroudi2007@gmail.com>
2026-03-28 19:26:37 +0000

    is there anything in the website that indicates that the website is unde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import About from "@/components/landing/about";
+import Blog from "@/components/landing/blog";
 import Faq from "@/components/landing/faq";
 import Footer from "@/components/landing/footer";
 import Header from "@/components/landing/header";
 import Hero from "@/components/landing/hero";
+import OurProcess from "@/components/landing/our-process";
 import Projects from "@/components/landing/projects";
 import Services from "@/components/landing/services";
+import Team from "@/components/landing/team";
+import Testimonials from "@/components/landing/testimonials";
 
 export default function Home() {
   return (
@@ -14,7 +18,11 @@ export default function Home() {
         <Hero />
         <About />
         <Services />
+        <OurProcess />
         <Projects />
+        <Testimonials />
+        <Team />
+        <Blog />
         <Faq />
       </main>
       <Footer />
